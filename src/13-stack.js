6e247c77aa71f59fd641d9ea4bcb40b62c0121ba
croftik <Chayka_Lory@mail.ru>
2021-04-12 23:30:35 +0300

    fix: complete task

--- src/13-stack.js
+++ src/13-stack.js
@@ -1,26 +1,23 @@
-/**
- * Implement the Stack with a given interface via array.
- *
- * @example
- * const stack = new Stack();
- *
- * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
- * stack.pop(); // returns the top element from stack and deletes it, returns 1
- * stack.pop(); // undefined
- *
- */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  constructor() {
+    this.items = [];
+  }
+
+  push(item) {
+    this.items.push(item);
   }
 
   pop() {
-    throw new Error('Not implemented');
+    let delItem;
+    if (this.items.length !== 0) {
+      delItem = this.items[this.items.length - 1];
+      this.items.pop();
+    }
+    return delItem;
   }
 
   peek() {
-    throw new Error('Not implemented');
+    return this.items[this.items.length - 1];
   }
 }
 
