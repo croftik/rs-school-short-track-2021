8e745206998670bb696ea079cc937feac250259a
croftik <Chayka_Lory@mail.ru>
2021-04-12 12:49:12 +0300

    fix: complete task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -1,15 +1,16 @@
-/**
- * Given a string, return its encoding version.
- *
- * @param {String} str
- * @return {String}
- *
- * @example
- * For aabbbc should return 2a3bc
- *
- */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 0;
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) count++;
+    else if (count === 0) result += str[i];
+    else {
+      count++;
+      result += count + str[i];
+      count = 0;
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
