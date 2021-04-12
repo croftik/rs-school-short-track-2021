ce6a3783868435b49ed88ec65bc9be19314e54ec
croftik <Chayka_Lory@mail.ru>
2021-04-12 11:55:36 +0300

    fix: complete task

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -1,17 +1,9 @@
-/**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
- *
- */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let result = n;
+  while (result >= 10) {
+    result = result.toString().split('').map((element) => Number(element)).reduce((accumulator, currentValue) => accumulator + currentValue);
+  }
+  return result;
 }
 
 module.exports = getSumOfDigits;
